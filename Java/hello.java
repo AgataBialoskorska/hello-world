659b3af7f0f829512e489139f685be7216995085
AgataBialoskorska <agatamirk@gmail.com>
2021-10-31 21:01:57 +0100

    add Java example

---
+++ Java/hello.java
@@ -0,0 +1,9 @@
+public class hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello from Java");
+    }
+}
+/* to compile - javac hello.java
+to run - java hello */
