b91cfe2a41a49f826f96e8f00a90578cac660c36
AgataBialoskorska <agatamirk@gmail.com>
2021-10-31 21:00:14 +0100

    add C# example

---
+++ C#/hello.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HelloCs
+{
+    class Hello {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from C# ");
+        }
+    }
+}
+/* to compile - 1. brew install mono
+2. mcs hello.cs
+to run - mono hello.exe */
